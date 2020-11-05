ff72ab5c72a8e0486ca1e7631ba004f72818c2a1
slasch <slavaschmidt@gmx.de>
2020-11-05 17:19:51 +0100

    Crypto WIP

--- src/main/scala/ton/sdk/client/modules/Crypto.scala
+++ src/main/scala/ton/sdk/client/modules/Crypto.scala
@@ -6,20 +6,19 @@ object Crypto {
 
   val prefix = "crypto"
 
-  val DEFAULT_MNEMONIC_DICTIONARY = 1
-  val DEFAULT_MNEMONIC_WORD_COUNT = 12
+  val DEFAULT_MNEMONIC_DICTIONARY   = 1
+  val DEFAULT_MNEMONIC_WORD_COUNT   = 12
   val DEFAULT_HDKEY_DERIVATION_PATH = "m/44'/396'/0'/0/0"
 
-
-  val MNEMONIC_DICTIONARY_TON = 0
-  val MNEMONIC_DICTIONARY_ENGLISH = 1
-  val MNEMONIC_DICTIONARY_CHINESE_SIMPLIFIED = 2
+  val MNEMONIC_DICTIONARY_TON                 = 0
+  val MNEMONIC_DICTIONARY_ENGLISH             = 1
+  val MNEMONIC_DICTIONARY_CHINESE_SIMPLIFIED  = 2
   val MNEMONIC_DICTIONARY_CHINESE_TRADITIONAL = 3
-  val MNEMONIC_DICTIONARY_FRENCH = 4
-  val MNEMONIC_DICTIONARY_ITALIAN = 5
-  val MNEMONIC_DICTIONARY_JAPANESE = 6
-  val MNEMONIC_DICTIONARY_KOREAN = 7
-  val MNEMONIC_DICTIONARY_SPANISH = 8
+  val MNEMONIC_DICTIONARY_FRENCH              = 4
+  val MNEMONIC_DICTIONARY_ITALIAN             = 5
+  val MNEMONIC_DICTIONARY_JAPANESE            = 6
+  val MNEMONIC_DICTIONARY_KOREAN              = 7
+  val MNEMONIC_DICTIONARY_SPANISH             = 8
 
   object Request {
     case object GenerateRandomSignKeys
@@ -35,8 +34,24 @@ object Crypto {
     case class MnemonicFromRandom(dictionary: Int, word_count: Int)
     case class MnemonicFromEntropy(entropy: String)
     case class MnemonicVerify(phrase: String, word_count: Int = DEFAULT_MNEMONIC_WORD_COUNT, dictionary: Int = DEFAULT_MNEMONIC_DICTIONARY)
-    case class MnemonicDeriveSignKeys(phrase: String, path: String = DEFAULT_HDKEY_DERIVATION_PATH, word_count: Int = DEFAULT_MNEMONIC_WORD_COUNT, dictionary: Int = DEFAULT_MNEMONIC_DICTIONARY)
+    case class MnemonicDeriveSignKeys(
+      phrase: String,
+      path: String = DEFAULT_HDKEY_DERIVATION_PATH,
+      word_count: Int = DEFAULT_MNEMONIC_WORD_COUNT,
+      dictionary: Int = DEFAULT_MNEMONIC_DICTIONARY
+    )
+    case class ModularPower(base: String, exponent: String, modulus: String)
+    case object NaclBoxKeyPair
+    case class NaclBoxKeyPairFromSecretKey(secret: String)
+    case class NaclBox(decrypted: String, nonce: String, their_public: String, secret: String)
+    case class NaclBoxOpen(encrypted: String, nonce: String, their_public: String, secret: String)
+    case class NaclSecretBox(decrypted: String, nonce: String, key: String)
+    case class NaclSecretBoxOpen(encrypted: String, nonce: String, key: String)
+    case class NaclSign(unsigned: String, secret: String)
+    case class NaclSignOpen(signed: String, public: String)
+    case class NaclSignDetached(unsigned: String, secret: String)
   }
+
   object Result {
     case class TonPublicKey(ton_public_key: String)
     case class Factors(factors: Seq[String])
@@ -53,6 +68,12 @@ object Crypto {
     }
     case class Validity(valid: Boolean)
     case class PublicSafe(public_safe: String)
+    case class ModularPower(modular_power: String)
+    case class Encrypted(encrypted: String)
+    case class Decrypted(decrypted: String)
+    case class Signed(signed: String)
+    case class Unsigned(unsigned: String)
+    case class Signature(signature: String)
   }
 
   import io.circe.generic.auto._
@@ -73,25 +94,25 @@ object Crypto {
   implicit val hdkeySecretFromXprv   = new SdkCall[Request.HdkeySecretFromXprv, Result.SecretKey] { override val functionName: String = s"$prefix.hdkey_secret_from_xprv"   }
   implicit val hdkeyXprvFromMnemonic = new SdkCall[Request.HdkeyXprvFromMnemonic, Result.Xprv]    { override val functionName: String = s"$prefix.hdkey_xprv_from_mnemonic" }
 
-  implicit val mnemonicWords       = new SdkCall[Request.MnemonicWords, Result.MnemonicWords]        { override val functionName: String = s"$prefix.mnemonic_words"        }
-  implicit val mnemonicFromRandom  = new SdkCall[Request.MnemonicFromRandom, Result.MnemonicPhrase]  { override val functionName: String = s"$prefix.mnemonic_from_random"  }
-  implicit val mnemonicFromEntropy = new SdkCall[Request.MnemonicFromEntropy, Result.MnemonicPhrase] { override val functionName: String = s"$prefix.mnemonic_from_entropy" }
-  implicit val mnemonicVerify      = new SdkCall[Request.MnemonicVerify, Result.Validity]            { override val functionName: String = s"$prefix.mnemonic_verify"       }
+  implicit val mnemonicWords          = new SdkCall[Request.MnemonicWords, Result.MnemonicWords]        { override val functionName: String = s"$prefix.mnemonic_words"            }
+  implicit val mnemonicFromRandom     = new SdkCall[Request.MnemonicFromRandom, Result.MnemonicPhrase]  { override val functionName: String = s"$prefix.mnemonic_from_random"      }
+  implicit val mnemonicFromEntropy    = new SdkCall[Request.MnemonicFromEntropy, Result.MnemonicPhrase] { override val functionName: String = s"$prefix.mnemonic_from_entropy"     }
+  implicit val mnemonicVerify         = new SdkCall[Request.MnemonicVerify, Result.Validity]            { override val functionName: String = s"$prefix.mnemonic_verify"           }
+  implicit val mnemonicDeriveSignKeys = new SdkCall[Request.MnemonicDeriveSignKeys, Result.PublicKey]   { override val functionName: String = s"$prefix.mnemonic_derive_sign_keys" }
+  implicit val modularPower           = new SdkCall[Request.ModularPower, Result.ModularPower]          { override val functionName: String = s"$prefix.modular_power"             }
+  implicit val naclBoxKeypair         = new SdkCall[Request.NaclBoxKeyPair.type, Result.SignKeys]       { override val functionName: String = s"$prefix.nacl_box_keypair"          }
+  implicit val naclBoxKeypairFromSecretKey = new SdkCall[Request.NaclBoxKeyPairFromSecretKey, Result.SignKeys] {
+    override val functionName: String = s"$prefix.nacl_box_keypair_from_secret_key"
+  }
 
-  implicit val mnemonicDeriveSignKeys = new SdkCall[Request.MnemonicDeriveSignKeys,Result.PublicKey] { override val functionName: String = s"$prefix.mnemonic_derive_sign_keys" }
-
-
-//  implicit val modular_power = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.modular_power" }
-//  implicit val nacl_box = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.nacl_box" }
-//  implicit val nacl_box_keypair = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.nacl_box_keypair" }
-//  implicit val nacl_box_keypair_from_secret_key = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.nacl_box_keypair_from_secret_key" }
-//  implicit val nacl_box_open = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.nacl_box_open" }
-//  implicit val nacl_secret_box = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.nacl_secret_box" }
-//  implicit val nacl_secret_box_open = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.nacl_secret_box_open" }
-//  implicit val nacl_sign = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.nacl_sign" }
-//  implicit val nacl_sign_detached = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.nacl_sign_detached" }
+  implicit val naclBox           = new SdkCall[Request.NaclBox, Result.Encrypted]           { override val functionName: String = s"$prefix.nacl_box"             }
+  implicit val naclBoxOpen       = new SdkCall[Request.NaclBoxOpen, Result.Decrypted]       { override val functionName: String = s"$prefix.nacl_box_open"        }
+  implicit val naclSecretBox     = new SdkCall[Request.NaclSecretBox, Result.Encrypted]     { override val functionName: String = s"$prefix.nacl_secret_box"      }
+  implicit val naclSecretBoxOpen = new SdkCall[Request.NaclSecretBoxOpen, Result.Decrypted] { override val functionName: String = s"$prefix.nacl_secret_box_open" }
+  implicit val naclSign          = new SdkCall[Request.NaclSign, Result.Signed]             { override val functionName: String = s"$prefix.nacl_sign"            }
+  implicit val naclSignOpen      = new SdkCall[Request.NaclSignOpen, Result.Unsigned]       { override val functionName: String = s"$prefix.nacl_sign_open"       }
+  implicit val naclSignDetached  = new SdkCall[Request.NaclSignDetached, Result.Signature]  { override val functionName: String = s"$prefix.nacl_sign_detached"   }
 //  implicit val nacl_sign_keypair_from_secret_key = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.nacl_sign_keypair_from_secret_key" }
-//  implicit val nacl_sign_open = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.nacl_sign_open" }
 
 //  implicit val scrypt = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.scrypt" }
 //  implicit val sha256 = new SdkCall[Request.,Result.] { override val functionName: String = s"$prefix.sha256" }
--- src/test/scala/ton/sdk/client/modules/CryptoSpec.scala
+++ src/test/scala/ton/sdk/client/modules/CryptoSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.matchers._
 import ton.sdk.client.modules.Api.SdkClientError
 import ton.sdk.client.modules.Crypto._
 import ton.sdk.client.modules.Context._
-import ton.sdk.client.modules.Crypto.Result.Validity
+import ton.sdk.client.modules.Crypto.Result.{Signature, Validity}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -224,7 +224,7 @@ class CryptoSpec[T[_]] extends AsyncFlatSpec with SdkAssertions[Future] {
   it should "mnemonic_derive_sign_keys 1" in {
     val result = local { implicit ctx =>
       for {
-        keypair <- call(Request.MnemonicDeriveSignKeys(phrase, dictionary = MNEMONIC_DICTIONARY_TON, word_count = 24))
+        keypair    <- call(Request.MnemonicDeriveSignKeys(phrase, dictionary = MNEMONIC_DICTIONARY_TON, word_count = 24))
         publicSafe <- call(Request.PublicKey(keypair.public))
       } yield publicSafe
     }
@@ -234,7 +234,7 @@ class CryptoSpec[T[_]] extends AsyncFlatSpec with SdkAssertions[Future] {
   it should "mnemonic_derive_sign_keys 2" in {
     val result = local { implicit ctx =>
       for {
-        keypair <- call(Request.MnemonicDeriveSignKeys(phrase, path = "m", dictionary = MNEMONIC_DICTIONARY_TON, word_count = 24))
+        keypair    <- call(Request.MnemonicDeriveSignKeys(phrase, path = "m", dictionary = MNEMONIC_DICTIONARY_TON, word_count = 24))
         publicSafe <- call(Request.PublicKey(keypair.public))
       } yield publicSafe
     }
@@ -245,7 +245,7 @@ class CryptoSpec[T[_]] extends AsyncFlatSpec with SdkAssertions[Future] {
     val phrase = "abandon math mimic master filter design carbon crystal rookie group knife young"
     val result = local { implicit ctx =>
       for {
-        keypair <- call(Request.MnemonicDeriveSignKeys(phrase))
+        keypair    <- call(Request.MnemonicDeriveSignKeys(phrase))
         publicSafe <- call(Request.PublicKey(keypair.public))
       } yield publicSafe
     }
@@ -255,12 +255,149 @@ class CryptoSpec[T[_]] extends AsyncFlatSpec with SdkAssertions[Future] {
   it should "mnemonic_derive_sign_keys 4" in {
     val result = local { implicit ctx =>
       for {
-        mnemonic <-call(Request.MnemonicFromEntropy("2199ebe996f14d9e4e2595113ad1e627"))
-        keypair <- call(Request.MnemonicDeriveSignKeys(mnemonic.phrase))
+        mnemonic   <- call(Request.MnemonicFromEntropy("2199ebe996f14d9e4e2595113ad1e627"))
+        keypair    <- call(Request.MnemonicDeriveSignKeys(mnemonic.phrase))
         publicSafe <- call(Request.PublicKey(keypair.public))
       } yield publicSafe
     }
     assertValue(result)(Result.TonPublicKey("PuZdw_KyXIzo8IksTrERN3_WoAoYTyK7OvM-yaLk711sUIB3"))
   }
 
+  it should "modular_power" in {
+    val result = local { implicit ctx =>
+      call(Request.ModularPower("0123456789ABCDEF", "0123", "01234567"))
+    }
+    assertValue(result)(Result.ModularPower("63bfdf"))
+  }
+
+  it should "not modular_power" in {
+    val result = local { implicit ctx =>
+      call(Request.ModularPower("1", "0123", "0.2"))
+    }
+    assertSdkError(result)("Invalid big int [0.2]")
+  }
+
+  it should "nacl_box_keypair" in {
+    val result = local { implicit ctx =>
+      call(Request.NaclBoxKeyPair)
+    }
+    assertExpression(result)(r => r.public.length == 64 && r.secret.length == 64 && r.public != r.secret)
+  }
+
+  it should "nacl_box_keypair_from_secret_key" in {
+    val result = local { implicit ctx =>
+      call(Request.NaclBoxKeyPairFromSecretKey("e207b5966fb2c5be1b71ed94ea813202706ab84253bdf4dc55232f82a1caf0d4"))
+    }
+    assertValue(result)(
+      Result.SignKeys("a53b003d3ffc1e159355cb37332d67fc235a7feb6381e36c803274074dc3933a", "e207b5966fb2c5be1b71ed94ea813202706ab84253bdf4dc55232f82a1caf0d4")
+    )
+  }
+
+  it should "not nacl_box_keypair_from_secret_key" in {
+    val result = local { implicit ctx =>
+      call(Request.NaclBoxKeyPairFromSecretKey("Top secret"))
+    }
+    assertSdkError(result)("Invalid secret key [Invalid character 'T' at position 0]: Top secret")
+  }
+
+  val nonce1      = "cd7f99924bf422544046e83595dd5803f17536f5c9a11746"
+  val theirPublic = "c4e2d9fe6a6baf8d1812b799856ef2a306291be7a7024837ad33a8530db79c6b"
+  val secret      = "d9b9dc5033fb416134e5d2107fdbacab5aadb297cb82dbdcd137d663bac59f7f"
+
+  it should "not nacl_box" in {
+    val result = local { implicit ctx =>
+      call(Request.NaclBox("0x00", nonce1, "", secret))
+    }
+    assertSdkError(result)("Invalid key size 0. Expected 32.")
+  }
+
+  it should "nacl_box and nacl_box_open and not nacl_box_open" in {
+    val decrypted = "TG9uZyBsaXZlIEZyZWUgVE9O"
+    val result = local { implicit ctx =>
+      for {
+        box    <- call(Request.NaclBox(decrypted, nonce1, theirPublic, secret))
+        opened <- call(Request.NaclBoxOpen(box.encrypted, nonce1, theirPublic, secret))
+      } yield (box, opened)
+    }
+    assertValue(result.map(_._2))(Result.Decrypted(decrypted))
+    val error = local { implicit ctx =>
+      call(Request.NaclBoxOpen(fe.unsafeGet(result)._1.encrypted, nonce1, theirPublic, ""))
+    }
+    assertSdkError(error)("Invalid key size 0. Expected 32.")
+  }
+
+  val key    = "8f68445b4e78c000fe4d6b7fc826879c1e63e3118379219a754ae66327764bd8"
+  val nonce2 = "2a33564717595ebe53d91a785b9e068aba625c8453a76e45"
+
+  it should "nacl_secret_box and nacl_secret_box_open" in {
+    val decrypted = "eyBMb25nIGxpdmUgRnJlZSBUT04gd2l0aCBzcGVjaWFsIGNoYXJzICcgIiB9ICQ9LD8gXQ=="
+    val result = local { implicit ctx =>
+      for {
+        box    <- call(Request.NaclSecretBox(decrypted, nonce2, secret))
+        opened <- call(Request.NaclSecretBoxOpen(box.encrypted, nonce2, secret))
+      } yield (box, opened)
+    }
+    assertValue(result.map(_._1))(Result.Encrypted("Q+jzxpEfrW3Zm13eohpaPNKNLxM7FeZXQG5nB5yIPWGsa0YEqBlnA45KdgqWB4AiR4QSjFxnM/W3Z3Bh7NN+WFQwNkM="))
+    assertValue(result.map(_._2))(Result.Decrypted(decrypted))
+  }
+
+  it should "not nacl_secret_box" in {
+    val result = local { implicit ctx =>
+      call(Request.NaclSecretBox("0x00", nonce2, ""))
+    }
+    assertSdkError(result)("Invalid key size 0. Expected 32.")
+  }
+
+  it should "not nacl_secret_box_open" in {
+    val result = local { implicit ctx =>
+      call(Request.NaclSecretBoxOpen("encrypted", nonce2, secret))
+    }
+    assertSdkError(result)("Invalid base64 string: Encoded text cannot have a 6-bit remainder.\r\nbase64: [encrypted]")
+  }
+
+  val signSecret       = "56b6a77093d6fdf14e593f36275d872d75de5b341942376b2a08759f3cbae78f1869b7ef29d58026217e9cf163cbfbd0de889bdf1bf4daebf5433a312f5b8d6e"
+  val naclSignExpected = Result.Signed("i7MMLJ8a5vUdNPenG6pV2JFC8KeE0EeEera9NmxRe+wazZ1Jx4h8rhMlyIagPRW8zYemO4RAocJefrBxG+XmCExvbmcgbGl2ZSBGcmVlIFRPTg==")
+  val unsigned         = "TG9uZyBsaXZlIEZyZWUgVE9O"
+
+  it should "nacl_sign" in {
+    val result = local { implicit ctx =>
+      call(Request.NaclSign(unsigned, signSecret))
+    }
+    assertValue(result)(naclSignExpected)
+  }
+
+  it should "not nacl_sign" in {
+    val result = local { implicit ctx =>
+      call(Request.NaclSign("You shall not pass!!!", signSecret))
+    }
+    assertSdkError(result)("Invalid base64 string: Encoded text cannot have a 6-bit remainder.\r\nbase64: [You shall not pass!!!]")
+  }
+
+  it should "nacl_sign_open" in {
+    val result = local { implicit ctx =>
+      call(Request.NaclSignOpen(naclSignExpected.signed, "1869b7ef29d58026217e9cf163cbfbd0de889bdf1bf4daebf5433a312f5b8d6e"))
+    }
+    assertValue(result)(Result.Unsigned(unsigned))
+  }
+
+  it should "not nacl_sign_open" in {
+    val result = local { implicit ctx =>
+      call(Request.NaclSignOpen("0==", signSecret))
+    }
+    assertSdkError(result)("Invalid base64 string: Invalid byte 61, offset 1.\r\nbase64: [0==]")
+  }
+
+  it should "nacl_sign_detached" in {
+    val result = local { implicit ctx =>
+      call(Request.NaclSignDetached(unsigned, signSecret))
+    }
+    assertValue(result)(Signature("8bb30c2c9f1ae6f51d34f7a71baa55d89142f0a784d047847ab6bd366c517bec1acd9d49c7887cae1325c886a03d15bccd87a63b8440a1c25e7eb0711be5e608"))
+  }
+
+  it should "not nacl_sign_detached" in {
+    val result = local { implicit ctx =>
+      call(Request.NaclSignDetached("OOOOoooooopppppps", signSecret))
+    }
+    assertSdkError(result)("Invalid base64 string: Encoded text cannot have a 6-bit remainder.\r\nbase64: [OOOOoooooopppppps]")
+  }
 }
