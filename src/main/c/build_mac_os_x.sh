4cde37687ae3b4ad5ae1638338fa09097950642f
slasch <slavaschmidt@gmx.de>
2021-02-17 09:07:34 +0100

    intel

--- src/main/c/build_mac_os_x.sh
+++ src/main/c/build_mac_os_x.sh
@@ -2,6 +2,9 @@
 
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 export LIBS="../resources"
+export SUFFIX="x86_64"
+# "aarch64"
+
 gcc \
   -dynamiclib \
   -shared \
@@ -9,13 +12,13 @@ gcc \
   -I$JAVA_HOME/include/darwin \
   -I$JAVA_HOME/include/linux \
   *.c \
-  -o $LIBS/libTonSdkClientJniBinding_aarch64.dylib \
+  -o "$LIBS/libTonSdkClientJniBinding_$SUFFIX.dylib" \
   -L$LIBS \
-  -lton_client_aarch64 \
+  -l"ton_client_$SUFFIX" \
   -Wl,-rpath,.
 
 cd $LIBS
 
-install_name_tool -change "libton_client_aarch64.dylib" @loader_path/libton_client_aarch64.dylib "libTonSdkClientJniBinding_aarch64.dylib"
+install_name_tool -change "libton_client_$SUFFIX.dylib" "@loader_path/libton_client_$SUFFIX.dylib" "libTonSdkClientJniBinding_$SUFFIX.dylib"
 
 cd -
